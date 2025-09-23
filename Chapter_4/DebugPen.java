ae62879b51bb048c1be86dd0bc7352661feec5b5
Manexy4 <211676424+CharlesKevinSalabsab-UCM@users.noreply.github.com>
2025-09-24 07:56:03 +0800

    p

--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -7,18 +7,15 @@ public class DebugPen {
         point = "fine";
     }
 
-
     public DebugPen(String color, String point) {
         this.color = color; 
         this.point = point; 
     }
 
-    // Getter for color
     public String getColor() {
         return color;
     }
 
-    // Getter for point
     public String getPoint() {
         return point;
     }
