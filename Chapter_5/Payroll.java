8293637dc120b67c79b7ba347a1d8df0505c84c7
Manexy4 <211676424+CharlesKevinSalabsab-UCM@users.noreply.github.com>
2025-10-12 21:29:07 +0800

    Create Payroll.java

---
+++ Chapter_5/Payroll.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Payroll
+{
+    public static void main (String args[])
+    {
+        int totalHours;
+        int payRate;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many hours did you work this week?");
+        totalHours = scanner.nextInt();
+        System.out.println("What is your regular pay rate?");
+        payRate = scanner.nextInt();
+
+
+        try {
+            Employee employee = new Employee(totalHours, payRate);
+            double regularPay = employee.calculateRegularHours(totalHours);
+            double overtimePay = employee.calculateOverHoursTime(totalHours);
+            System.out.println("Regular pay is " + regularPay);
+            System.out.println("Overtime pay is " + overtimePay);
+        }
+        catch (Exception e) {
+          System.out.println(e);
+        }
+    }
+}
