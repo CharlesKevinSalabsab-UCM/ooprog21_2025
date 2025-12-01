29a7e76eda9d68b62275abd84422a8016fa0e0f1
Manexy4 <211676424+CharlesKevinSalabsab-UCM@users.noreply.github.com>
2025-12-01 08:22:23 +0800

    Create Chapter_8/ArraysDemo.java

---
+++ Chapter_8/ArraysDemo.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ArraysDemo {
+    
+    public static void display(String message, int[] array) {
+        System.out.println(message);
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println(); 
+    }
+
+    public static void main(String[] args) {
+    
+        int[] arr = new int[5];
+
+        display("Original array:", arr);
+
+        Arrays.fill(arr, 8);
+        display("After filling with 8s:", arr);
+
+        arr[2] = 6;
+        arr[4] = 3;
+        display("After changing two values:", arr);
+
+        Arrays.sort(arr);
+        display("After sorting:", arr);
+    }
+}
